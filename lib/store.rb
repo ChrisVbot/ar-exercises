43340f96556772391012bd4a9261dbdefe9d0b00
ChrisVbot <chris.veillette@gmail.com>
2016-08-03 20:56:20 +0000

    added custom validator

--- lib/store.rb
+++ lib/store.rb
@@ -4,5 +4,13 @@ class Store < ActiveRecord::Base
 
   validates :name, length:{minimum: 3}
   validates :annual_revenue, numericality: true, allow_nil: false
+  validates :must_have_mens_or_womens_apparel, confirmation:true
 
+
+  def must_have_mens_or_womens_apparel
+    unless :mens_apparel.present? || :womens_apparel.present?
+      errors.add("Must carry men or women's apparel")
+    end
+  end
 end
+
